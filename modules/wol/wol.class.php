033b06afe901968d1eb109b49cbcdb84a2e307b7
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2018-10-28 18:20:21 +0500

    Update wol.class.php

--- modules/wol/wol.class.php
+++ modules/wol/wol.class.php
@@ -139,20 +139,137 @@ function wake($mac='D0:50:99:54:24:DD') {
 flush();
 
 // Port number where the computer is listening. Usually, any number between 1-50000 will do. Normally people choose 7 or 9.
-$socket_number = "7";
+//$socket_number = "7";
 // MAC Address of the listening computer's network device
 
 // IP address of the listening computer. Input the domain name if you are using a hostname (like when under Dynamic DNS/IP)
 //$ip_addy = gethostbyname("myhomeserver.dynamicdns.org");
-$ip_addy = "192.168.1.63";
+//$ip_addy = "192.168.1.63";
 
-$res=$this->WakeOnLan($ip_addy, $mac,$socket_number);
+//$res=$this->WakeOnLan($ip_addy, $mac,$socket_number);
+
+//$res=$this->WakeOnLan("255.255.255.255", $mac);
+$res=$this->WakeOnLan("192.168.1.255", $mac);
 return $res;
 
 }
 
+function wakeOnLan($broadcast, $mac)
+{
+    $mac_array = explode(':', $mac);
 
-function WakeOnLan($addr, $mac,$socket_number) {
+    $hwaddr = '';
+
+    foreach($mac_array AS $octet)
+    {
+        $hwaddr .= chr(hexdec($octet));
+    }
+
+    // Create Magic Packet
+
+    $packet = '';
+    for ($i = 1; $i <= 6; $i++)
+    {
+        $packet .= chr(255);
+    }
+
+    for ($i = 1; $i <= 16; $i++)
+    {
+        $packet .= $hwaddr;
+    }
+
+    $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+    if ($sock)
+    {
+        $options = socket_set_option($sock, 1, 6, true);
+
+        if ($options >=0) 
+        {    
+            $e = socket_sendto($sock, $packet, strlen($packet), 0, $broadcast, 7);
+            socket_close($sock);
+        }    
+    }
+}
+
+
+function wakeOnLan7($ipAddr,$macAddr) {
+    // makeMagicPacket
+    $magicPacket = "";
+    for ($i=0; $i<6; $i++) {
+        $magicPacket.= chr(0xff);
+    }
+    $aryMacAddr = explode(":", $macAddr, 6);
+    $buffer = pack("H*H*H*H*H*H*", $aryMacAddr[0], $aryMacAddr[1], $aryMacAddr[2],
+    $aryMacAddr[3], $aryMacAddr[4], $aryMacAddr[5]);
+    for ($i=0; $i<16; $i++) {
+        $magicPacket.= $buffer;
+    }
+ 
+    // makeBroadCastAddr
+    $aryIpAddr = explode(".", $ipAddr, 4);
+    if ($aryIpAddr[0] < 127) {
+        $aryIpAddr[1] = "255";
+    }
+    if ($aryIpAddr[0] < 191) {
+        $aryIpAddr[2] = "255";
+    }
+    if ($aryIpAddr[0] < 223) {
+        $aryIpAddr[3] = "255";
+    }
+    $broadCastAddr = join(".", $aryIpAddr);
+    // send
+    $fp = fsockopen("udp://".$broadCastAddr, 2304, $errno, $errstr);
+    if (!$fp) {
+        print("ERROR: $errno - $errstr\n");
+    } else {
+        fwrite($fp, $magicPacket);
+        fwrite($fp, $magicPacket);
+        fclose($fp);
+    }
+ }
+
+
+function WakeOnLan6($broadcast, $mac_addres){
+$package = chr(255).chr(255).chr(255).chr(255).chr(255).chr(255);
+$parts_mac = explode(':', $mac_addres);
+for ($i=0; $i < 6; $i++) $transform_mac .= chr(hexdec($parts_mac[$i]));
+for($i = 1; $i <= 16; $i++) $package .= $transform_mac;
+$port=80;
+$wol = fsockopen("udp://$broadcast", $port);
+fwrite($wol, $package);
+fclose($wol);
+
+}
+
+
+
+function WakeOnLan3($broadcast, $mac){
+$mac_array = preg_split('#:#', $mac); //print_r($mac_array);
+$hwaddr = '';
+    foreach($mac_array AS $octet){
+    $hwaddr .= chr(hexdec($octet));
+    }
+    //Magic Packet
+    $packet = '';
+    for ($i = 1; $i <= 6; $i++){
+    $packet .= chr(255);
+    }
+    for ($i = 1; $i <= 16; $i++){
+    $packet .= $hwaddr;
+    }
+    //set up socket
+    $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+    if ($sock){
+    $options = socket_set_option($sock, 1, 6, true);
+        if ($options >=0){    
+        $e = socket_sendto($sock, $packet, strlen($packet), 0, $broadcast, 7);
+        socket_close($sock);
+        }    
+    }
+}  
+
+
+function WakeOnLan1($addr, $mac,$socket_number) {
   $addr_byte = explode(':', $mac);
   $hw_addr = '';
   for ($a=0; $a <6; $a++) $hw_addr .= chr(hexdec($addr_byte[$a]));
@@ -164,27 +281,23 @@ function WakeOnLan($addr, $mac,$socket_number) {
   if ($s == false) {
     $res= "Error creating socket!\n";
     $res.= "Error code is '".socket_last_error($s)."' - " . socket_strerror(socket_last_error($s));
-
-   $out['RESULT']=$res;
     return $res;
     } else {
     // setting a broadcast option to socket:
     $opt_ret = socket_set_option($s, 1, 6, TRUE);
     if($opt_ret <0) {
     $res= "setsockopt() failed, error: " . strerror($opt_ret) . "\n";
-   $out['RESULT']=$res;
 
-      return FALSE;      }
+      return $res;
+      }
     if(socket_sendto($s, $msg, strlen($msg), 0, $addr, $socket_number)) {
 
     $res= "Magic Packet sent successfully!";
-//   $out['RESULT']=$res;
-
       socket_close($s);
       return $res;
       }   else {      
     $res= "Magic packet failed!";
-   $out['RESULT']=$res;
+
 
       return $res;
       }   }  }
